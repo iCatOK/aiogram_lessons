6013711a167e528fa8956f1ef998cf3b2929d0de
iCatOK <kamilyen5@gmail.com>
2023-11-04 18:49:39 +0500

    feat(bot): add env variables for token

--- lesson1/bot.py
+++ lesson1/bot.py
@@ -1,4 +1,11 @@
+import os
 
+from dotenv import load_dotenv, find_dotenv
+
+name = "psycotricks"
+
+load_dotenv(find_dotenv())
+token = os.environ.get("TOKEN")
 
 if __name__ == '__main__':
-    print('aaaa')
+    print(token)
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import logging
+import os
+
+from dotenv import load_dotenv
+
+# logging config
+logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
+
+# load environment
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    logging.info('Environment loaded')
